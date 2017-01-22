ab96ae0aaa12d06d0dd3a7c1e29e70d497bbaf3d
Wan Zulkarnain <wanzulkarnain69@gmail.com>
2017-01-22 09:00:21 +0800

    Update callback.php

--- controllers/front/callback.php
+++ controllers/front/callback.php
@@ -70,9 +70,10 @@ class BillplzCallbackModuleFrontController extends ModuleFrontController {
         $data = $billplz->check_bill($api_key, $id, $mode);
         if ($data['paid']) {
 
+            $amount = number_format(($data['amount'] / 100), 2);
             // Check for possible fake form request. If fake, stop
             $signature = isset($_GET['signature']) ? $_GET['signature'] : 'No Valid Signature';
-            $this->checkDataIntegrity($signature, $api_key, $collection_id, $data['name']);
+            $this->checkDataIntegrity($signature, $api_key, $collection_id, $data['name'], $amount);
 
             // Tak boleh guna $this->context sebab ini server side
             $cart = new Cart($data['reference_1']);
@@ -151,9 +152,9 @@ class BillplzCallbackModuleFrontController extends ModuleFrontController {
      * Signature using MD5, combination of API Key and Customer Email
      */
 
-    private function checkDataIntegrity($signature, $api_key, $collection_id, $name) {
+    private function checkDataIntegrity($signature, $api_key, $collection_id, $name, $amount) {
 
-        $new_signature = md5($api_key . $collection_id . strtolower($name));
+        $new_signature = md5($api_key . $collection_id . strtolower($name) . $amount);
 
         if ($signature != $new_signature)
             die('Invalid Request. Reason: Invalid Signature');
